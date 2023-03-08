a57e7c31740ec2fb16d4cd40e4a6ea2f4634188a
shrikant Lohar <shrikantlohar91@gmail.com>
2023-03-08 10:02:26 +0530

    ...

--- src/MobileTestLibrary/SetupUtils.py
+++ src/MobileTestLibrary/SetupUtils.py
@@ -8,7 +8,8 @@ class SetupUtils:
     @keyword
     def start_appium_service(self):
         appium_service = AppiumService()
-        appium_service.start(args=['--address', '127.0.0.1', '-p', str(DEFAULT_PORT), '--base-path', '/wd/hub/'])
+        appium_service.start(
+            args=['--address', '127.0.0.1', '-p', str(DEFAULT_PORT), '--base-path', '/wd/hub/'])
         assert appium_service.is_running
         assert appium_service.is_listening
 
@@ -31,8 +32,6 @@ class SetupUtils:
     #     except:
     #         raise print("No emulator running...")
 
-
-
     # @keyword
     # def start_emulator(self, emulator_name, deviceType):
     #     if deviceType == 'virtual':
@@ -43,4 +42,3 @@ class SetupUtils:
     #         print('Started emulator...')
     #     else:
     #         print("Testing started")
-
