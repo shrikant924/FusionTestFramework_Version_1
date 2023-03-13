4475b6c685449f84214f609a86e5d4ebff0c829d
shrikant Lohar <shrikantlohar91@gmail.com>
2023-03-13 12:31:02 +0530

    updated

--- com.playgroundxyz.vision_project/runner.py
+++ com.playgroundxyz.vision_project/runner.py
@@ -5,7 +5,7 @@ from robot import run
 
 
 def runTest():
-    reportDirectory = os.path.join('../com.playgroundxyz.vision_project/com.playgroundxyz.vision_project.TestResults/',
+    reportDirectory = os.path.join('../FusionTestFramework/com.playgroundxyz.vision_project/com.playgroundxyz.vision_project.TestResults/',
                                    datetime.now().strftime('%d-%m-%y_%H-%M-%S'))
     os.makedirs(reportDirectory)
     try:
